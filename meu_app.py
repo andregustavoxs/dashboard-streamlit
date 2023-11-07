6c6d2142cd3cc3db303a0724acef9224c2bf68dc
andregustavoxs <andrewinstitucional@gmail.com>
2023-11-07 08:27:58 -0300

    added initial codes

---
+++ meu_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title = "Meu site Streamlit")
+
+with st.container():
+    st.subheader("Meu primeiro site com o Streamlit")
+    st.title("Dashboard de contratos")
+    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo da vida.")
+    st.write("Quer aprender Python? [Clique Aqui](https://www.hashtagtreinamentos.com/curso-python)")
+
+with st.container():
+    st.write("---")
